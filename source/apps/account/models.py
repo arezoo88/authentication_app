216d878e2239027a05b2fc9ef9eacb36a8efe7a8
Arezoo <arezoo.darvishi69@yahoo.com>
2024-08-08 11:18:53 +0330

    remove extra import

--- source/apps/account/models.py
+++ source/apps/account/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from apps.account.utils import is_valid_mobile
 
 
 class UserManager(BaseUserManager):
